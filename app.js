52dc3c667e99a961499e7ed60deb89be2bfa69fc
Joe Kurleto <joedoku@gmail.com>
2013-03-06 12:43:31 -0500

    Express finally serving socket.io js correctly

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
